f269c0149339757fe2b2b398c55806c1259290a0
naufalzs <naufal.zufar.s@gmail.com>
2022-05-07 17:41:25 +0700

    checkpoint sidebar

--- src/App.js
+++ src/App.js
@@ -19,7 +19,19 @@ function App() {
           </ul>
         </div>
       </div>
-      <div className="main-content"></div>
+      <div className="main-content">
+        <div className="main-left"></div>
+        <div className="main-right">
+          <h3>Diskusi 5 Teratas</h3>
+          <ul className="trending">
+            <li><div className="num-box">1</div>Cara mengetahui akun Facebook di-hack melalui aplikasi</li>
+            <li><div className="num-box">1</div>Batas aman overclock PC rakitan</li>
+            <li><div className="num-box">1</div>Cara mengetahui akun Facebook di-hack melalui aplikasi</li>
+            <li><div className="num-box">1</div>Cara mengetahui akun Facebook di-hack melalui aplikasi</li>
+            <li><div className="num-box">1</div>Cara mengetahui akun Facebook di-hack melalui aplikasi</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
