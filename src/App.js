d7718eb3aa962454cbb325045f2757d9a24f7eee
naufalzs <naufal.zufar.s@gmail.com>
2022-05-07 13:06:15 +0700

    checkpoint navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="navbar">
+        <div className="navbar-content">
+          <div className="navbar-left">
+            <h1 style={{ fontSize: 32 }}>Forum Anak IT</h1>
+            <div>Search Bar</div>
+          </div>
+          <ul className="navbar-right">
+            <li>Categories</li>
+            <li>Login</li>
+            <li>Register</li>
+          </ul>
+        </div>
+      </div>
+      <div className="main-content"></div>
     </div>
   );
 }
