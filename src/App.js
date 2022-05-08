a174cb2ddb32d8798539584c4c707d4f65ff4541
naufalzs <naufal.zufar.s@gmail.com>
2022-05-08 09:17:55 +0700

    pairing dummy json data

--- src/App.js
+++ src/App.js
@@ -5,9 +5,12 @@ import {
   faArrowUp,
   faArrowDown,
 } from "@fortawesome/free-solid-svg-icons";
-import {Comments} from "./json/comments.json"
+import { format } from "date-fns";
+import comments from "./json/comments.json";
+import trendings from "./json/trendings.json";
 
 function App() {
+  let dateFormat = (date) => format(new Date(date), "dd MMMM yyyy hh:mm");
   return (
     <div className="container">
       <div className="navbar">
@@ -45,35 +48,56 @@ function App() {
           </div>
           <div className="section-comment">
             <h2 className="title-1">Komentar</h2>
-            <div className="comment-container">
-              {/* <div className="pic"></div> */}
-              <img src="https://picsum.photos/200" alt="" className="pic" />
-              <div className="comment-content">
-                <h4>Neal Topham</h4>
-                <div className="comment-date">08 February 2017 07.30</div>
-                <p>
-                  Mungkin ada fenomena paranormal yang tidak bisa dijelaskan.
-                  Lebih baik nyala mati sendiri daripada tidak nyala sama sekali
-                </p>
-                <div className="comment-vote-container">
-                  <p>3 point</p>
-                  <div className="vote-square">
-                    <FontAwesomeIcon icon={faArrowUp} className="vote-icon" />
+            {/* <div className="pic"></div> */}
+            {comments?.map((item) => (
+              <div key={item?.id} className="comment-container">
+                <img src={item?.avatar} alt="" className="pic" />
+                <div className="comment-content">
+                  <h4>{item?.author}</h4>
+                  <div className="comment-date">{dateFormat(item?.date)}</div>
+                  <p>{item?.message}</p>
+                  <div className="comment-vote-container">
+                    <p>{item?.point} point</p>
+                    <div className="vote-square">
+                      <FontAwesomeIcon icon={faArrowUp} className="vote-icon" />
+                    </div>
+                    <div className="vote-square">
+                      <FontAwesomeIcon
+                        icon={faArrowDown}
+                        className="vote-icon"
+                      />
+                    </div>
                   </div>
-                  <div className="vote-square">
-                    <FontAwesomeIcon icon={faArrowDown} className="vote-icon" />
-                  </div>
-                </div>
-                <div className="reply-container">
-                  <div className="pic-2"></div>
-                  <div className="comment-content">
-                    <h4>Dyan Shahkin</h4>
-                    <div className="comment-date">08 February 2017 07.30</div>
-                    <p>Confirm, itu kemungkinan aktivitas paranormal.</p>
-                  </div>
+                  {item?.replies?.map((reply) => (
+                    <div key={reply?.id} className="reply-container">
+                      <img src={reply?.avatar} alt="" className="pic-2" />
+                      <div className="comment-content">
+                        <h4>{reply?.author}</h4>
+                        <div className="comment-date">
+                          {dateFormat(reply?.date)}
+                        </div>
+                        <p>{reply?.message}</p>
+                        <div className="comment-vote-container">
+                          <p>{reply?.point} point</p>
+                          <div className="vote-square">
+                            <FontAwesomeIcon
+                              icon={faArrowUp}
+                              className="vote-icon"
+                            />
+                          </div>
+                          <div className="vote-square">
+                            <FontAwesomeIcon
+                              icon={faArrowDown}
+                              className="vote-icon"
+                            />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  ))}
                 </div>
               </div>
-            </div>
+            ))}
           </div>
           <div className="section-comment-input">
             <h3 className="title-2">Tambahkan komentar</h3>
@@ -110,25 +134,12 @@ function App() {
         <div className="main-right">
           <h3>Diskusi 5 Teratas</h3>
           <ul className="trending">
-            <li>
-              <div className="num-box">1</div>Cara mengetahui akun Facebook
-              di-hack melalui aplikasi
-            </li>
-            <li>
-              <div className="num-box">1</div>Batas aman overclock PC rakitan
-            </li>
-            <li>
-              <div className="num-box">1</div>Cara mengetahui akun Facebook
-              di-hack melalui aplikasi
-            </li>
-            <li>
-              <div className="num-box">1</div>Cara mengetahui akun Facebook
-              di-hack melalui aplikasi
-            </li>
-            <li>
-              <div className="num-box">1</div>Cara mengetahui akun Facebook
-              di-hack melalui aplikasi
-            </li>
+            {trendings?.map((trending) => (
+              <li key={trending?.id}>
+                <div className="num-box">{trending?.id}</div>
+                {trending?.topic}
+              </li>
+            ))}
           </ul>
         </div>
       </div>
